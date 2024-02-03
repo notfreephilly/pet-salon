be9e5972d041a10454816235e2de100ed518ae99
philip yoder <notphilyoder@philips-MacBook-Pro.local>
2024-02-03 14:39:31 -0800

    Object literal

---
+++ scripts/register.js
@@ -0,0 +1,28 @@
+let salon = {
+    name: "Good Dawgz",
+    phone: "123-234-3423",
+    address: {
+        street: "good boy ave",
+        number: "321",
+        zip: "4939039"
+    },
+    pets: [
+        {
+            name: "Bruce",
+            age: 6
+        },
+        {
+            name: "Woofy",
+            age: "Unknown"
+        }
+    ] 
+}
+
+
+
+
+// function displayFooterInfo() {
+//     document.getElementById('info').innerHTML = `
+//     <p>Welcome to ${salon.name}. Happy to have you</p>`
+// }
+// displayFooterInfo();
---
+++ scripts/script.js
@@ -0,0 +1,39 @@
+var globalVariable = "I'm in a global scope";
+
+function myFunction() {
+    // This function can access globalVariable
+    console.log(globalVariable);
+}
+myFunction();
+
+
+function myFunction2() {
+    var localVariable = "I'm in a local scope";
+    console.log(localVariable);
+
+    if (true) {
+        let blockVariable = "I'm in a block scope";
+        console.log(blockVariable);
+    }
+}
+// Accessing blockVariable here would result in an error
+myFunction2();
+
+
+function planeTicket(destination, price) {
+    console.log("Traveling to " + destination);
+    return price * 1.11;
+}
+
+
+function calculateProfits() {
+    var price1 = planeTicket("Hawaii", 550);
+    var price2 = planeTicket("Argentina", 1200);
+    var price3 = planeTicket("Germany", 800);
+    
+    console.log(price1 + price2 + price3);
+}
+calculateProfits();
+
+
+
