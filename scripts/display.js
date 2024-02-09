584904a58902e46f24c6f882557c96205dfb1724
philip yoder <notphilyoder@philips-MacBook-Pro.local>
2024-02-08 18:04:55 -0800

    added new functionality and style

---
+++ scripts/display.js
@@ -0,0 +1,30 @@
+function displayPetNames() {
+    getE('pets').innerHTML = "";
+    for (let i = 0; i < salon.pets.length; i++) {
+        getE('pets').innerHTML += `<p>${salon.pets[i].name}</p>`;
+    }
+    getE('totalPets').innerHTML = `Total`
+}
+
+
+
+
+
+function displayPetCards() {
+    getE('pets').innerHTML = "";
+    let card = ""
+    for (let i = 0; i < salon.pets.length; i++){
+        let pet = salon.pets[i];
+        card = `
+            <div class="petCard">
+                <p>Name: ${pet.name}</p>
+                <p>Age: ${pet.age}</p>
+                <p>Gender: ${pet.gender}</p>
+            
+            </div>
+        `;
+        console.log(card);
+    }
+    getE('pets').innerHTML = card;
+}
+
--- scripts/register.js
+++ scripts/register.js
@@ -5,59 +5,66 @@ let salon = {
     address: {
         street: "Woof Paw Rd",
         number: "393",
-        zip: "40282"
+        zip: "40282",
     },
-    pets: [
-        {
-            name: "Bruce",
-            age: 6,
-            gender: "Male",
-            service: false,
-            breed: "Pitbull, mixed"
-        },
-        {
-            name: "Woofy",
-            age: 3,
-            gender: "Male",
-            service: true,
-            breed: "French bulldog"
-        },
-        {
-            name: "Charley",
-            age: 7,
-            gender: "Female",
-            service: true,
-            breed: "Golden Retriever"
-        }
-    ] 
+    pets: [] 
 }
 
-// When a user clicks this button, it will run the totalPetsRegistered function below
+// constructor
 
-let registeredResultBtn = document.getElementById('woof-btn').addEventListener('click', totalPetsRegistered)
-
-// totalPetsRegistered function is going into the HTML, looking for an element with the ID of 'registered-result' and display the value. In this case, within the template literal is an expression that goes into the salon object and gets the length of the pets array.
-// Outputs 3
-
-function totalPetsRegistered() {
-    document.getElementById('registered-result').innerHTML = `We have ${salon.pets.length} lovely pets registred with us!`;
+function Pet(name, age, gender, breed, service, type) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    this.breed = breed;
+    this.service = service;
+    this.type = type;
 }
 
-// This button is listening for a user to click. When the button is clicked, getNamesDisplayBtn will exectute the displayPetNames function
 
-let petNamesDisplayedBtn = document.getElementById('meow-btn').addEventListener('click', displayPetNames);
 
-// Declaring and initializing furryNames 
-
-const furryNames = document.getElementById('name-result');
-
-// This function first initializes the variable names with an empty string
-// Then it goes to the pets array and for each element with the key name "name" and returns the value to the furryNames variable. In this case it will display in the element with the ID of "name-result"
-
-function displayPetNames() {
-    let names = "";
-    salon.pets.forEach(e => 
-        names += `<p>${e.name}</p>`
-    );
-    furryNames.innerHTML = names;
+function getE(id) {
+    return document.getElementById(id);
 }
+
+
+
+// get elements 
+let inputName = getE('txtName');
+let inputAge = getE('txtAge');
+let inputGender = getE('txtGender');
+let inputBreed = getE('txtBreed');
+let inputService = getE('txtService');
+let inputType = getE('txtType');
+
+function register() {
+    let newPet = new Pet(
+        inputName.value,
+        inputAge.value,
+        inputGender.value,
+        inputBreed.value,
+        inputService.value,
+        inputType.value
+        )
+    salon.pets.push(newPet);
+    
+    displayPetCards();
+        
+        // clear the input
+
+        
+}
+    
+
+// OTHER FUNCTIONS
+    
+    
+    function init() {
+            let pet1 = new Pet("Bruce", 6, "Male");
+            let pet2 = new Pet("Charley", 12, "Female");
+            let pet3 = new Pet("Shelly", 2, "Male");
+        
+        salon.pets.push(pet3, pet4, pet5, pet6)
+        displayPetCards();
+}
+        
